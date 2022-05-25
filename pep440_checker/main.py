33c3ab2fc9c0a20b1d0951540e58b354c0b0e023
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2022-05-25 16:13:36 +0900

    #1 Add version range checker

--- pep440_checker/main.py
+++ pep440_checker/main.py
@@ -1,10 +1,15 @@
 import asyncio
 
+pyscript = pyscript  # noqa: F821
+Element = Element  # noqa: F821
+
 
 def _import_dependencies():
-    from poetry.core.version import pep440
+    from poetry.core.semver.version import Version
+    from poetry.core.semver.helpers import parse_constraint
     from poetry.core.version.exceptions import InvalidVersion
-    return pep440, InvalidVersion
+    from poetry.core.semver.exceptions import ParseConstraintError
+    return Version, parse_constraint, InvalidVersion, ParseConstraintError
 
 
 async def import_dependencies():
@@ -21,21 +26,51 @@ class PEP440:
         pass
 
     async def validate_version(self, version):
-        pep440, InvalidVersion = await import_dependencies()
+        Version, _, InvalidVersion, _ = await import_dependencies()
         try:
-            pep440.PEP440Version.parse(version)
+            Version.parse(version)
             return True
         except InvalidVersion:
             return False
+
+    async def is_in(self, version, version_range):
+        (
+            Version, parse_constraint, InvalidVersion, ParseConstraintError
+        ) = await import_dependencies()
+        try:
+            version = Version.parse(version)
+            version_range = parse_constraint(version_range)
+            return version_range.allows(version)
+        except InvalidVersion:
+            return f"Invalid version: '{version}'"
+        except ParseConstraintError:
+            return f"Invalid version range: '{version_range}'"
 
 
 async def test_version(event):
     version = Element("version-to-test").element.value  # noqa: F821
     is_valid = await PEP440().validate_version(version)
-    pyscript.write(  # noqa: F821
+    pyscript.write(
         "version-test-result",
-        f"{version} is" + (" " if is_valid else " not ") + "valid version",
+        f"Version <code>{version}</code> is"
+        + (" " if is_valid else " not ")
+        + "valid versioning",
     )
+
+
+async def test_version_range(event):
+    version = Element("version-specification").element.value
+    version_range = Element("version-range").element.value
+    is_in = await PEP440().is_in(version, version_range)
+    if isinstance(is_in, str):
+        pyscript.write("version-range-result", is_in)
+    else:
+        pyscript.write(
+            "version-range-result",
+            f"Version <code>{version}</code> is"
+            + (" " if is_in else " not ")
+            + f"in range <code>{version_range}</code>",
+        )
 
 
 async def main():
